f6152d4a916834ad34d20c8399399563a8ac609d
martaf793 <martaf@uchicago.edu>
2025-04-14 20:36:14 +0000

    users_number

--- app.rb
+++ app.rb
@@ -7,3 +7,23 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square/results") do
+  @n=params.fetch("users_number").to_f
+  @result=n**2
+  erb(:square_results)
+end
+
+# get("/square_root/new") do
+#   erb(:square_root_new)
+# end
+# get("/random/new") do
+#   erb(:random_new)
+# end
+# get("/payment/new") do
+#   erb(:payment_new)
+# end
