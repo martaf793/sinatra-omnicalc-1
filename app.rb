496a47b32c7c5494613c7c55c989c042477d3f87
martaf793 <martaf@uchicago.edu>
2025-04-14 21:23:44 +0000

    almost

--- app.rb
+++ app.rb
@@ -21,6 +21,12 @@ end
 get("/square_root/new") do
   erb(:square_root_new)
 end
+get("/square/root/results") do
+  @users_n=params.fetch("users_n").to_f
+  @result=@users_n**0.5
+  erb(:square_root_results)
+end
+
 get("/random/new") do
   erb(:random_new)
 end
@@ -30,6 +36,17 @@ get("/random/results") do
   @random_n=rand(@r1..@r2)
 erb(:random_results)
 end
-# get("/payment/new") do
-#   erb(:payment_new)
-# end
+get("/payment/new") do
+  erb(:payment_new)
+end
+
+get("/payment/results") do
+  @apr=params.fetch("apr").to_fs(:percentage,{:precision=>4})
+  @apr_m=@apr/100/12
+  @years=params.fetch("years").to_i*12
+  @ppal=params.fetch("ppal").to_fs(:currency,{:precision=>2})
+  @numerator= @apr_m*@ppal
+  @denominator= 1-(1+@apr_m)**(-@years)
+  @result=@numerator/@denominator
+  erb(:payment_results)
+end
